3bf23662f5bbe57973f79578518484aae8107574
click2donatemm <developer.click2donatemm@gmail.com>
2024-12-11 14:17:26 +0700

    new update

---
+++ app/exhibition/page.tsx
@@ -0,0 +1,555 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+export default function Exhibition() {
+  return (
+    <main className="relative w-full">
+      <div className="max-w-5xl mx-auto pb-20 md:pb-32 min-h-screen h-screen snap-y snap-mandatory overflow-y-scroll">
+        <section className="snap-center min-h-screen relative flex justify-center items-center">
+          <h2 className="text-4xl text-yellow-400">
+            Welcome to our exhibition
+          </h2>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/01.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex justify-center items-center">
+          <div className="flex">
+            <Image
+              src="/images/02.jpg"
+              alt="background Image"
+              width={500}
+              height={300}
+              className=""
+            />
+            <Image
+              src="/images/03.jpg"
+              alt="background Image"
+              width={500}
+              height={300}
+              className=""
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/04.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/05.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/06.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/07.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/08.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/09.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/10.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/11.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex justify-center items-center">
+          <div className="flex">
+            <Image
+              src="/images/12.jpg"
+              alt="background Image"
+              width={500}
+              height={300}
+              className=""
+            />
+            <Image
+              src="/images/13.jpg"
+              alt="background Image"
+              width={500}
+              height={300}
+              className=""
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/14.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/15.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/16.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/17.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/18.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/19.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/20.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/21.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex justify-center items-center">
+          <div className="flex">
+            <Image
+              src="/images/22.jpg"
+              alt="background Image"
+              width={500}
+              height={300}
+              className=""
+            />
+            <Image
+              src="/images/23.jpg"
+              alt="background Image"
+              width={500}
+              height={300}
+              className=""
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/24.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/25.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/26.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/27.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/28.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/29.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/30.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/31.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex justify-center items-center">
+          <div className="flex">
+            <Image
+              src="/images/32.jpg"
+              alt="background Image"
+              width={500}
+              height={300}
+              className=""
+            />
+            <Image
+              src="/images/33.jpg"
+              alt="background Image"
+              width={500}
+              height={300}
+              className=""
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/34.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/35.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/36.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/37.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/38.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/39.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/40.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/41.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex justify-center items-center">
+          <div className="flex">
+            <Image
+              src="/images/42.jpg"
+              alt="background Image"
+              width={500}
+              height={300}
+              className=""
+            />
+            <Image
+              src="/images/43.jpg"
+              alt="background Image"
+              width={500}
+              height={300}
+              className=""
+            />
+            <Image
+              src="/images/44.jpg"
+              alt="background Image"
+              width={500}
+              height={300}
+              className=""
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/45.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/46.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/47.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/48.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/49.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/50.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/51.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/52.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/53.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>{" "}
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/54.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+        <section className="snap-center min-h-screen relative flex">
+          <div className="bg-wrap">
+            <Image
+              src="/images/55.jpg"
+              fill
+              alt="background Image"
+              className="fixed-bg object-cover h-auto transition-all duration-300"
+            />
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
